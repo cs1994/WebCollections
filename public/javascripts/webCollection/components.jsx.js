70b68c8de90d313a55c9b15165d5f97f402bed31
caoshuai <caoshuai@neotel.com.cn>
2016-04-13 18:21:15 +0800

    register

--- public/javascripts/webCollection/components.jsx.js
+++ public/javascripts/webCollection/components.jsx.js
@@ -20,9 +20,22 @@
         },
         render:function(){
             return(
-                <div>
-                    <input type="text" id="emailInput" />
-                    <button onClick={this.submit} style={{width:"40px",height:"40px"}}></button>
+                <div className="register">
+                    <div className="registerHeader">
+                       <div style={{width:"50%",display:"inline-block"}}>
+                           <p style={{borderRight:"3px solid #777"}}>收藏</p>
+                           <p>注册</p>
+                       </div>
+                        <div style={{width:"50%",display:"inline-block",textAlign:"right"}}>
+                            <p style={{fontSize:"18px"}}>已有账号？</p>
+                            <button>登录</button>
+                        </div>
+                    </div>
+                    <div className="registerContent">
+                        <p > 请输入您要注册的邮箱</p>
+                        <input type="text" id="emailInput" placeholder="邮箱"/>
+                        <button onClick={this.submit} >同意协议并注册</button>
+                    </div>
                 </div>
             )
         }
